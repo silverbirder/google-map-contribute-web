ee42c594c87c1f41498faed8c23546aaba463981
shibamoto <silverbirder@gmail.com>
2024-06-01 15:06:06 +0900

    feat: hover and clickable

--- src/app/components/ContributorAvatar.tsx
+++ src/app/components/ContributorAvatar.tsx
@@ -7,13 +7,19 @@ const ContributorAvatar = forwardRef<HTMLDivElement>((_, ref) => {
   const contributorUrl =
     "https://lh3.googleusercontent.com/a/ACg8ocLeQGLmPuxu9fYa4_s7VKUEzQ8Zp-rb8rbHGcNsvm3wufpgptYwEg=w72-h72-p-rp-mo-ba6-br100";
 
+  const handleClick = () => {
+    console.log("Contributor avatar clicked");
+  };
+
   return (
     <motion.div
       ref={ref}
-      className="absolute"
+      className="absolute flex items-center justify-center cursor-pointer"
       initial={{ scale: 0, opacity: 0 }}
       animate={{ scale: 1, opacity: 1 }}
       transition={{ duration: 0.5 }}
+      whileHover={{ scale: 1.2 }}
+      onClick={handleClick}
       style={{ top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}
     >
       <BorderBeam size={100} className="h-10 w-10 rounded-full border-2" />
--- src/app/components/PlaceAvatar.tsx
+++ src/app/components/PlaceAvatar.tsx
@@ -12,14 +12,18 @@ const PlaceAvatar = forwardRef<HTMLDivElement, PlaceAvatarProps>(
   ({ position, index }, ref) => {
     const placeUrl =
       "https://lh5.googleusercontent.com/p/AF1QipMICgecR9K6cLgvj1MY25MeGp6lKNSBd6rRsSj3=w72-h72-p-k-no-rp-br100";
-
+    const handleClick = () => {
+      console.log(`Place ${index + 1} avatar clicked`);
+    };
     return (
       <motion.div
         ref={ref}
-        className="absolute"
+        className="absolute cursor-pointer"
         initial={{ scale: 0, opacity: 0 }}
         animate={{ scale: 1, opacity: 1 }}
         transition={{ duration: 0.5 }}
+        whileHover={{ scale: 1.2 }}
+        onClick={handleClick}
         style={{ top: position.y, left: position.x }}
       >
         <BorderBeam size={100} className="h-10 w-10 rounded-full border-2" />
