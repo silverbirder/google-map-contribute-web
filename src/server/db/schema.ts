2beb24a2df56760bb86eb416951e14772e0b08ea
shibamoto <silverbirder@gmail.com>
2024-09-03 22:44:35 +0900

    fix: build

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,15 +1,12 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
 import {
-  index,
   integer,
   pgTableCreator,
   serial,
   text,
   timestamp,
-  varchar,
 } from "drizzle-orm/pg-core";
 
 /**
